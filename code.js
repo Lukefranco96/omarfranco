7110462493bd8ff775a6bac0b396f2bacde14ef3
Lukefranco96 <143677538+Lukefranco96@users.noreply.github.com>
2024-01-23 00:04:09 -0600

    subimos primer commit Portafolio omar V 1.0.0

---
+++ code.js
@@ -0,0 +1,23 @@
+let tabLinks  = document.getElementsByClassName("tab-links");
+let tabContents  = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link")
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+    };
+
+  
+
+const sidemenu = document.getElementById("sidemenu");
+  function openmenu(){
+    sidemenu.style.right= "0";
+  }
+  function closemenu(){
+    sidemenu.style.right= "-200px";
+  };
---
+++ form.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail= new PHPMailer(true);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['full-name'];
+    $mailDir = $_POST['email'];
+    
+    $mensaje = $_POST['message'];
+try{
+   
+    $mail-> isSMTP();
+    $mail -> Host = 'smtp-mail.outlook.com';
+    $mail -> SMTPAuth= true;
+    $mail-> Username = 'donino55@hotmail.com';
+    $mail-> Password = 'Joaf1996';
+    $mail -> SMTPSecure = PHPMailer:: ENCRYPTION_STARTTLS;
+    $mail -> Port = 587;
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    $mail-> setFrom('donino55@hotmail.com');
+    $mail  -> addAddress ('jomaragfc@outlook.com');
+    $mail -> isHTML(true);
+    $mail -> Subject= 'Persona interesada en ti ';
+    $mail->Body = 'Nombre: ' . $nombre . '<br>Email: ' . $mailDir .'<br>Mensaje: ' . $mensaje;;
+    $mail -> send();
+
+     // redireccion
+    
+    echo '<script> alert("El correo se envio de forma exitosa!")</script>';
+    header('Location: index.html');
+}catch (Exception $e){
+    echo 'MEnsaje '. $mail -> ErrorInfo;
+}
+}
+?>
